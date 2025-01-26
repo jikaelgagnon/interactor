bdb84690ec5dfd29037d2fc51fd1353dc0019734
jikael <jikaelgagnon@gmail.com>
2025-01-26 14:15:20 -0500

    Add todos

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 console.log("the script started!");
+// TODO: Come up with a general way to list elements to track and
+// how to mark them. Currently, i think the easiest way is to add the interactor class
+// to each of them.
+// Current problem: listing every single element by id can become very cumbersome. Also,
+// What about off chance that website already has an interactor class?
+// Probably want a set to track all relevant elements.
 
 var elementsToTrack = [
 	{
