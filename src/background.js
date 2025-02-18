b4840567717b6d673a2fe1c4bb3f635decd5810d
jikael <jikaelgagnon@gmail.com>
2025-02-18 12:38:19 -0500

    Fully functional with firebasegit statusgit status

--- src/background.js
+++ src/background.js
@@ -1,11 +1,9 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { collection, addDoc } from "firebase/firestore"; 
 import { db } from "./firebase.js";
 
 async function addToDB(document) {
   try {
-    const docRef = await addDoc(collection(db, "users"), document);
+    const docRef = await addDoc(collection(db, "userData"), document);
     console.log("Document written with ID: ", docRef.id);
   } catch (e) {
     console.error("Error adding document: ", e);
